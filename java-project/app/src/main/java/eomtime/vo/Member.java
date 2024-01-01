92f7983c7a73abbf36ccfe6a903c841a2534d59a
eomjinyoung <jinyoung.eom@gmail.com>
2024-01-01 10:13:14 +0900

    .

---
+++ java-project/app/src/main/java/eomtime/vo/Member.java
@@ -0,0 +1,10 @@
+package eomtime.vo;
+
+public class Member {
+  public String studentNo;
+  public String name;
+  public int age;
+  public boolean local;
+  public char gender;
+  public float average;
+}
